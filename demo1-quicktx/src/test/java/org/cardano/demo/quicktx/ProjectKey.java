31b19b16a89b9584121f487637c76100f0f8a15b
Satya <satran004@gmail.com>
2024-02-28 20:57:02 +0800

    Remove

--- demo1-quicktx/src/test/java/org/cardano/demo/quicktx/ProjectKey.java
+++ demo1-quicktx/src/test/java/org/cardano/demo/quicktx/ProjectKey.java
@@ -1,5 +1,5 @@
 package org.cardano.demo.quicktx;
 
 public interface ProjectKey {
-    String BLOCKFROST_PROJECT_ID = "preprodcE7NkLLZvWCAdXKgjARcBZRSn1jeZcQR";
+    String BLOCKFROST_PROJECT_ID = "";
 }
